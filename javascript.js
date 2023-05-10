7c781cc929fe857d68109902c671be2cf2da42b6
fadelyking <fadely195@gmail.com>
2023-05-10 13:41:54 +0300

    Everything works. Point system still not done.

--- javascript.js
+++ javascript.js
@@ -14,34 +14,77 @@ function getComputerChoice() {
     }
 }
 
-let userText = "paPEr";
-let computerSelection = getComputerChoice();
-let playerSelection = userText.charAt(0).toUpperCase() + userText.slice(1).toLowerCase();
 
-console.log(playerSelection)
 
-function singleRound(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
-        console.log("tie");
+    function game() {
+        let userText = prompt("Rock, Paper, Scissors?");
+        const playerSelection = userText.charAt(0).toUpperCase() + userText.slice(1).toLowerCase();
+        let computerSelection = getComputerChoice();
+        let aiScore = 0;
+        let humanScore = 0;
+        let winIndicator = 0;
 
-    } else if ((computerSelection === "Rock" && playerSelection === "Scissors") || 
-    (computerSelection === "Paper" && playerSelection === "Rock") || 
-    (computerSelection === "Scissors" && playerSelection === "Paper")) {
-
-        console.log(`You lose ! ${computerSelection} beats ${playerSelection}`); 
-    }  
+        function playRound(playerSelection, computerSelection) {
+        if (computerSelection === playerSelection) {
+            console.log(`Tie !`)
+            return 0
+             
+            
     
-    else if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
-    (playerSelection === "Paper" && computerSelection === "Rock") || 
-    (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        } else if ((computerSelection === "Rock" && playerSelection === "Scissors") || 
+        (computerSelection === "Paper" && playerSelection === "Rock") || 
+        (computerSelection === "Scissors" && playerSelection === "Paper")) {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+            return 1;
+        }  
+        
+        else if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
+        (playerSelection === "Paper" && computerSelection === "Rock") || 
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+            return 2;
+        } 
+    }
+const result = playRound();
+   if (result === 1) {
+    aiScore++;
+   } else if (result === 2) {
+    humanScore++;
+   } 
+    userText = prompt("Rock, Paper, Scissors?");
+    computerSelection = getComputerChoice();
 
-        console.log(`You win ! ${computerSelection} beats ${playerSelection}`); 
-    } 
-}
+    playRound(playerSelection, computerSelection);
+    if (result === 1) {
+        aiScore++;
+       } else if (result === 2) {
+        humanScore++;
+       } 
+    userText = prompt("Rock, Paper, Scissors?");
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    if (result === 1) {
+        aiScore++;
+       } else if (result === 2) {
+        humanScore++;
+       } 
+    userText = prompt("Rock, Paper, Scissors?");
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    if (result === 1) {
+        aiScore++;
+       } else if (result === 2) {
+        humanScore++;
+       } 
+    userText = prompt("Rock, Paper, Scissors?");
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    if (result === 1) {
+        aiScore++;
+       } else if (result === 2) {
+        humanScore++;
+       } 
 
+    }
 
-singleRound(computerSelection, playerSelection);
-
-function playRound(playerSelection, computerSelection) {
-
-  }
+game();
